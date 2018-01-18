a65e4c356de7ccb54444a6bbdaa5237fbaa478aa
Gual Escalante <gual23@gmail.com>
2018-01-17 18:16:36 -0600

    Added Lineups page

---
+++ src/pages/lineups/lineups.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { LineupsPage } from './lineups';
+
+@NgModule({
+  declarations: [
+    LineupsPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LineupsPage),
+  ],
+})
+export class LineupsPageModule {}
