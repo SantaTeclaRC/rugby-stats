fcaacd88fff18a390e6385a9e56384da4de476e5
Gual Escalante <gual23@gmail.com>
2018-02-22 19:03:40 -0600

    Added angularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LineupsPage } from '../pages/lineups/lineups';
@@ -11,6 +14,15 @@ import { LineupPage } from '../pages/lineup/lineup';
 import { RefereesPage } from '../pages/referees/referees';
 import { LiveMatchPage } from '../pages/live-match/live-match';
 import { TimerComponent } from '../components/timer/timer';
+
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
 
 @NgModule({
   declarations: [
@@ -24,7 +36,9 @@ import { TimerComponent } from '../components/timer/timer';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
