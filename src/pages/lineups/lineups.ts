e07c108eda16f79cadde68ad571c4015a0e16ea2
Gual Escalante <gual23@gmail.com>
2018-02-26 01:39:09 -0600

    Added data loading from params to lineups, passed params to single lineup page

--- src/pages/lineups/lineups.ts
+++ src/pages/lineups/lineups.ts
@@ -3,18 +3,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { LineupPage } from '../lineup/lineup';
 import { RefereesPage } from '../referees/referees';
 
+import { Team } from '../../models/team';
+
 @IonicPage()
 @Component({
   selector: 'page-lineups',
   templateUrl: 'lineups.html',
 })
 export class LineupsPage {
+  homeTeam: Team;
+  awayTeam: Team;
 
   homeRoot = LineupPage;
   awayRoot = LineupPage;
   refereeRoot = RefereesPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  homeParams = { team: null }
+  awayParams = { team: null }
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.homeTeam = this.navParams.get('home');
+    this.awayTeam = this.navParams.get('away');
+
+    this.homeParams.team = this.homeTeam
+    this.awayParams.team = this.awayTeam
+  }
 }
