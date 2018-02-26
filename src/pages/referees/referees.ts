9c0ae62b35c23c1c8126fcf6fb7c7ea2f8b77c2c
Gual Escalante <gual23@gmail.com>
2018-02-26 01:39:46 -0600

    Added remote data loading for referees

--- src/pages/referees/referees.ts
+++ src/pages/referees/referees.ts
@@ -3,19 +3,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { LiveMatchPage } from '../live-match/live-match';
 import { App } from 'ionic-angular/components/app/app';
 
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
 @IonicPage()
 @Component({
   selector: 'page-referees',
   templateUrl: 'referees.html',
 })
 export class RefereesPage {
+  referees: Observable<any>;
+  refereesList: AngularFireList<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App,
+    afDatabase: AngularFireDatabase) {
+    this.refereesList = afDatabase.list('/referees');
+    this.referees = this.refereesList.valueChanges();
   }
 
   goToLiveMatch () {
     let nav = this.app.getRootNav()
     nav.push(LiveMatchPage);
   }
-
 }
